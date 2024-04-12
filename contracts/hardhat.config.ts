b5eef985194e3c6d0ee22ec3b1560b75ed58bcb8
uok825 <omerleroyunda@gmail.com>
2024-04-13 01:01:14 +0200

    [contracts]feat: GasslessProxy FIX:Solidity Compiler Version

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,7 +3,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.20",
   networks: {
     "sapphire-testnet": {
       // This is Testnet! If you want Mainnet, add a new network config item.
