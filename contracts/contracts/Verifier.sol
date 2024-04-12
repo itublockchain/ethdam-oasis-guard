34df7a2943f135615ff32ca5cc198b9fe3db454f
uok825 <omerleroyunda@gmail.com>
2024-04-12 22:52:32 +0200

    [contracts] Account, AccountFactory, interfaces

--- contracts/contracts/Verifier.sol
+++ contracts/contracts/Verifier.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 import {Base64} from "./helpers/Base64.sol";
 import {VerifierCaller} from "./helpers/VerifierCaller.sol";
